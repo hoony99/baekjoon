bbc0063da504bb465febac54f7d064df1c78f251
hoony99 <122731357+hoony99@users.noreply.github.com>
2023-01-17 18:28:56 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.c
+++ 백준/Silver/13305. 주유소/주유소.c
@@ -6,50 +6,20 @@
 
 
 int main() {
-    int n, roadSum = 0, priceSum = 0, min = 10000;
+    int n, result = 0, min = 100000000 , cur;
     scanf("%d", &n);
     int* arr = (int*)malloc(sizeof(int) * n-1); // 도로 길이
-    int* arr2 = (int*)malloc(sizeof(int) * n); // 도시별 리터당 가격
+
     for (int i = 0; i < n-1; i++) { // 도로 길이
         scanf("%d", &arr[i]);
-        roadSum += arr[i];
     }
-    for (int i = 0; i < n; i++) { // 도시별 리터당 가격
-        scanf("%d", &arr2[i]);
-        if (min > arr2[i] && i != n-1) {
-            min = arr2[i];
-        }
-    }
+
     for (int i = 0; i < n-1; i++) {
-        if (i == 0) {
-            if (arr2[0] <= arr2[1]) {
-                if (arr2[0] == min) {
-                    priceSum += roadSum * arr2[0];
-                    break;
-                }
-                else {
-                    priceSum += (arr[0] + arr[1]) * arr2[0];
-                    roadSum -= (arr[0] + arr[1]);
-                }
-
-            }
-            else {
-                priceSum += (arr[0]) * arr2[0];
-                roadSum -= (arr[0]);
-            }
-        }
-        else {
-            if (arr2[i] == min) {
-                priceSum += (roadSum * min);
-                break;
-            }
-            else {
-                priceSum += arr[i] * arr2[i];
-                roadSum -= (arr[i]);
-            }
-        }
+        scanf("%d", &cur);
+        if (i == 0 || min > cur)
+            min = cur;
+        result += min * arr[i];
     }    
-
-    printf("%d", priceSum);
+    printf("%d", result);
     return 0;
 }
